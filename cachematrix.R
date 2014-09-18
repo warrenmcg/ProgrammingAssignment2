94f7ed935e1f00c4bd4194cb91be5e5518fcb799
warrenmcg <warren-mcgee@fsm.northwestern.edu>
2014-09-18 14:44:08 -0500

    fixed a typo and formatted the code to be more readable

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,69 @@
 ## The inversion of a matrix is a costly computation,
-## so these functions are designed to cache the inversion of a matrix
-## and return the cache if the matrix has not changed and the inversion ahs already been done
-## 
-## Created by Warren McGee for Coursera Programming Assignment 2 due 2014-09-21
+# so these 2 functions are designed to cache the inversion 
+# of a matrix and return the cache if the matrix has not 
+# changed and the inversion has already been done.
+# 
+# Created by Warren McGee for Coursera Programming Assignment 2 due 2014-09-21
+##
 
-## This function creates a special matrix that can store the cache of its inversion
+## makeCacheMatrix:
+# This function creates a special matrix that can store the cache of its inversion
+# NOTE: this function assumes that the matrix supplied is invertible!
+# Args: x = invertible matrix
+# Returns: cacheMatrix object with functions to store matrix and cache and change them
+##
 makeCacheMatrix <- function(x = matrix()) {
-	inv <- NULL # Initialize the inversion
-	set <- function(y) { # Replace old with new matrix; then remove the cache of the old one
+	
+	inv <- NULL  # Initialize the inversion
+	
+	# Replace old with new matrix; then remove the cache of the old one
+	set <- function(y) { 
 		x <<- y 
 		inv <<- NULL
 	}
-	get <- function() x # Return the matrix
-	setInv <- function(inversion) inv <<- inversion # Store the inversion cache in "inv"
-	getInv <- function() inv # Return the cached inversion or NULL if it doesn't exist
-	object <- list(set = set, get = get, setInv = setInv, getInv = getInv) # List of functions
-	class(object) <- "cacheMatrix" # set the class of the list of functions to "cacheMatrix"
-	object # Return a cacheMatrix object with matrix and ability to store a cached inversion
+	
+	# Function to return the matrix
+	get <- function() x  
+	# Function to store a cache of the inverse of 'x'
+	setInv <- function(inversion) inv <<- inversion  
+	# Function to return the cached inverse of 'x' (or NULL if it doesn't exist)
+	getInv <- function() inv
+	
+	# Put the above functions into a list to define an object to return
+	object <- list(set = set, get = get, setInv = setInv, getInv = getInv)
+	# set the class of above list to "cacheMatrix"
+	class(object) <- "cacheMatrix"
+	# Return the cacheMatrix object with matrix and ability to store a cached inversion
+	object
 }
 
 
-## This function calculates the inversion of a special matrix or returns the cache if it exists
+## cacheSolve:
+# This function calculates the inversion of cacheMatrix or returns the cache if it exists
+# Args: x = cacheMatrix; ... = other arguments
+# Returns: the inverse of x
+##
 cacheSolve <- function(x, ...) {
+	
 	# If x is not a cacheMatrix object, then the rest of the function won't work
 	if(!class(x)=="cacheMatrix") { 
 		stop("cacheSolve requires x to be a cacheMatrix object.") 
 	}
 	
-	inv <- x$getInv() # Get cache
+	inv <- x$getInv()  # Get cache
+	
 	# If cache exists, then return that instead of calculating it
 	if(!is.null(inv)) {
 		message("getting cached data")
 		return(inv)
 	}
+	
 	# If cache does not exist, then calculate it instead
-	matrix <- x$get() # get the Matrix
-	inv <- solve(matrix) # calculate the inversion using the solve() function
-	x$setInv(inv) # set the cache so that it can be used for the future
-	inv ## Return the matrix that is the inverse of 'x'
+	matrix <- x$get()  # get the Matrix
+	# calculate the inversion using the solve() function
+	inv <- solve(matrix) 
+	# set the cache so that it can be used for the future
+	x$setInv(inv)
+	# Return the matrix that is the inverse of 'x'
+	inv 
 }
